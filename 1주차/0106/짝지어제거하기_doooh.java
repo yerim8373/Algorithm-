4915db310e6ed26455b6092129cd9d2e1b2841cd
kimdoha <kkhhjj3393@gmail.com>
2023-01-08 23:44:43 +0900

    [doooh] 짝지어제거하기

--- 1주차/0106/짝지어제거하기_doooh.java
+++ 1주차/0106/짝지어제거하기_doooh.java
@@ -1,35 +1,29 @@
 import java.util.Stack;
 
-class Solution
-{
-    public int solution(String s)
-    {
-    int answer = 0;
+class Solution {
 
-    // 1. 문자열을 char 배열로 변환 & 스택 마련
-    char[] chars = s.toCharArray();
-    Stack<Character> stack = new Stack<>();
+    public int solution(String s) {
+      int answer = 0;
 
-    // 2. 문자열 제거 로직
-    for(char ch : chars){
-      // i. stack 에 값이 있는 경우
-      if(stack.size() > 0) {
-        if(stack.peek() == ch) {
+      // 1. 문자열을 char 배열로 변환 & 스택 마련
+      char[] chars = s.toCharArray();
+      Stack<Character> stack = new Stack<>();
+
+      // 2. 문자열 제거 로직
+      for(char ch : chars){
+        // i. stack 에 값이 있고, 동일한 문자가 있는 경우
+        if(stack.size() > 0 && stack.peek() == ch) {
           stack.pop();
-        } else {
-          stack.push(ch);
+          continue;
         }
-
-        // j. stack 이 비어 있는 경우
-      } else {
+        // ii. stack 이 비어있거나, 동일한 문자가 아닌 경우
         stack.push(ch);
       }
-    }
 
-    // 3. 짝지어모두제거(1), 아니면(0) 결과 반환
-    if(stack.size() == 0) answer = 1;
+      // 3. 짝지어모두제거(1), 아니면(0) 결과 반환
+      if(stack.size() == 0) answer = 1;
 
-    return answer;
+      return answer;
 
     }
 }
