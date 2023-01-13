cbdecba78205e588408b58d245e0254a89026df9
KimByeungHyun <qudgus05@naver.com>
2023-01-14 00:01:32 +0900

    [병현] 행렬의 곱셈

---
+++ 0주차/1226/프린터_sandbend.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+// 프로그래머스 프린터
+public class Main11 {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        PriorityQueue<Integer> priorityQueueHighest = new PriorityQueue<>(Collections.reverseOrder());
+
+        // 큐에다가 큰 숫자 숫서대로 추가
+        for (int i=0; i < priorities.length; i++) {
+            priorityQueueHighest.add(priorities[i]);
+        }
+
+        while (!priorityQueueHighest.isEmpty()) {
+
+            for (int i=0; i<priorities.length; i++) {
+                // 인쇄 대기목록과 최상위 큐가 같을 때
+                if (priorities[i] == priorityQueueHighest.peek()) {
+
+                    if (i == location) {
+                        answer++;
+                        return answer;
+                    }
+                    // 맨앞에 있는 값 반환후 삭제
+                    priorityQueueHighest.poll();
+                    answer++;
+                }
+            }
+        }
+        return -1;
+    }
+}
