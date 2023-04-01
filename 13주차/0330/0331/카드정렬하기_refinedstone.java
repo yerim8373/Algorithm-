c5202a278bcfdc3d402c02deb734bf7cb2daa160
F i n n <113455892+RefinedStone@users.noreply.github.com>
2023-04-01 16:46:06 +0900

    [refinedstone]카드정렬하기

---
+++ 13주차/0330/0331/카드정렬하기_refinedstone.java
@@ -0,0 +1,17 @@
+
+public class 카드정렬하기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        PriorityQueue<Integer> pq = IntStream.range(0, n).mapToObj(i -> sc.nextInt()).collect(Collectors.toCollection(PriorityQueue::new));
+        int answer = 0;
+        /*main-logic*/
+        while (pq.size() >= 2) {
+            int a = pq.poll();
+            int b = pq.poll();
+            pq.add(a + b);
+            answer = answer + a + b;
+        }
+        System.out.println(answer);
+    }
+}
