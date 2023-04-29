3c7ce2a0242a6c69a277c087b7e564019830c642
hellojihyoung <jihyoungkwon@gmail.com>
2023-04-30 01:08:25 +0900

    [hellojihyoung] 강의실 배정

---
+++ 17주차/0425/강의실 배정_hellojihyoung.py
@@ -0,0 +1,20 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+n = int(input())
+time = []
+for _ in range(n):
+    time.append(list(map(int, input().split())))
+
+time.sort()
+room = []
+
+heapq.heappush(room, time[0][1])
+for i in range(1, n):
+    if time[i][0] < room[0]:
+        heapq.heappush(room, time[i][1])
+    else:
+        heapq.heappop(room)
+        heapq.heappush(room, time[i][1])
+print(len(room))
